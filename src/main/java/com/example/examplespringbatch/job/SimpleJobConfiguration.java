c80576b480898b8b5dbb784e6875fd442a906682
SLCube <kgm0096@naver.com>
2023-06-27 21:14:54 +0900

    mysql database에 연결 및 batch_job_execution, batch_job_instance table 학습

--- src/main/java/com/example/examplespringbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/examplespringbatch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,18 +24,18 @@ public class SimpleJobConfiguration {
     public Job simpleJob() {
         // Job안에 여러개의 Step이 나뉘어짐
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
-                .next(simpleStep1())
-                .next(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
                     // 이곳에서 수행할 기능들을 명시함.
                     log.info(">>>> This is Step1");
+                    log.info(">>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
